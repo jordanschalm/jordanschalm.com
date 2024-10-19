6835c64956f8e8bbc73d50fcbea8bb3771681d34
Jordan Schalm <jordan.schalm@gmail.com>
2024-10-19 10:01:00 -0700

    tweak about blurb

--- src/components/AboutBlurb.tsx
+++ src/components/AboutBlurb.tsx
@@ -7,13 +7,15 @@ export default function AboutBlurb() {
         👋🏻 Hi, my name is Jordan. I'm a software engineer based in Vancouver,
         BC. I work on the Core Protocol team at the{' '}
         <A href="https://flow.com">Flow Foundation</A>, building fault-tolerant
-        distributed systems. Previously, I co-created{' '}
+        distributed systems.
+      </P>
+      <P>
+        Previously, I co-created{' '}
         <A href="https://www.cryptokitties.co">CryptoKitties</A> and taught for
         the{' '}
         <A href="https://masterdatascience.ubc.ca">Master of Data Science</A>{' '}
         program at <A href="https://ubc.ca">UBC</A>.
       </P>
-      <P></P>
     </>
   )
 }
