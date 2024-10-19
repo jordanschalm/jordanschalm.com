1376af2d0f30a28868e850c9c30c7cdbff0783b1
Jordan Schalm <jordan.schalm@gmail.com>
2024-10-19 15:32:14 -0700

    add fonts to tailwind

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,27 @@
-import type { Metadata } from 'next'
+import type { Metadata } from 'next' // this is important for some reason
 import './globals.css'
 import Terminal from '@/components/Terminal'
 import Nav from '@/components/Nav'
 import { Inconsolata, Josefin_Slab, Alfa_Slab_One } from 'next/font/google'
 
+const inconsolata = Inconsolata({
+  weight: ['400', '700'],
+  subsets: ['latin'],
+  variable: '--font-inconsolata',
+})
+
+const josefinSlab = Josefin_Slab({
+  weight: ['400', '700'],
+  subsets: ['latin'],
+  variable: '--font-josefin-slab',
+})
+
 // https://fonts.googleapis.com/css?family=Alfa+Slab+One|Josefin+Slab:400,700|Inconsolata:400,700
-
-const inconsolata = Inconsolata({ subsets: ['latin'] })
-
-export const metadata: Metadata = {
-  title: 'Jordan Schalm',
-  description: "Jordan Schalm's personal site",
-}
+const alfaSlabOne = Alfa_Slab_One({
+  weight: ['400'],
+  subsets: ['latin'],
+  variable: '--font-alfa-slab-one',
+})
 
 export default function RootLayout({
   children,
@@ -19,8 +29,11 @@ export default function RootLayout({
   children: React.ReactNode
 }>) {
   return (
-    <html lang="en">
-      <body className={inconsolata.className}>
+    <html
+      lang="en"
+      className={`${inconsolata.variable} ${josefinSlab.variable} ${alfaSlabOne.variable}`}
+    >
+      <body className="font-alfa">
         <div className="w-full flex flex-col items-center px-2 ">
           <header className="max-w-prose w-full my-4">
             <Terminal text="Jordan Schalm" />
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,19 +1,24 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss'
 
 const config: Config = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
+      fontFamily: {
+        sans: ['var(--font-inconsolata)'],
+        josefin: ['var(--font-josefin-slab)'],
+        alfa: ['var(--font-alfa-one-slab)'],
+      },
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        background: 'var(--background)',
+        foreground: 'var(--foreground)',
       },
     },
   },
   plugins: [],
-};
-export default config;
+}
+export default config
