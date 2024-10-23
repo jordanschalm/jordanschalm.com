80c20ffbe1a75e4a58e427719921bb46cfe3e1f1
Jordan Schalm <jordan.schalm@gmail.com>
2024-10-23 11:09:00 -0700

    show active navlink

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,25 +1,34 @@
-import Link from 'next/link'
+'use client'
 
-const NavItem = ({ children, href }) => (
-  <Link
-    className="
+import Link from 'next/link'
+import { usePathname } from 'next/navigation'
+
+const NavItem = ({ children, href }) => {
+  const path = usePathname()
+  return (
+    <Link
+      className={`
       px-4 py-1
       text-md font-thin tracking-widest uppercase text-slate-500
+      link ${href === path ? 'text-amber-700' : ''}
       hover:text-amber-700 hover:underline
-      underline-offset-4 decoration-2"
-    href={href}
-  >
-    {children}
-  </Link>
-)
+      underline-offset-4 decoration-2`}
+      href={href}
+    >
+      {children}
+    </Link>
+  )
+}
 
-const Nav = () => (
-  <nav className="flex justify-around font-alfa">
-    <NavItem href="/">/</NavItem>
-    <NavItem href="/now">Now</NavItem>
-    <NavItem href="/work">Work</NavItem>
-    <NavItem href="/blog">Blog</NavItem>
-  </nav>
-)
+const Nav = () => {
+  return (
+    <nav className="flex justify-around font-alfa">
+      <NavItem href="/">/</NavItem>
+      <NavItem href="/now">Now</NavItem>
+      <NavItem href="/work">Work</NavItem>
+      <NavItem href="/blog">Blog</NavItem>
+    </nav>
+  )
+}
 
 export default Nav
